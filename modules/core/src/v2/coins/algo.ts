514b19f44ab505c7bdc2e0e537ce3b4d3c589942
Luis Rojas <luis@bitgo.com>
2019-06-05 22:42:14 +0200

    CT-220 - Add ALGO key logic to create a wallet

--- modules/core/src/v2/coins/algo.ts
+++ modules/core/src/v2/coins/algo.ts
@@ -1,35 +1,115 @@
 import BaseCoin = require('../baseCoin');
+const algosdk = require('algosdk');
+
+interface KeyPair {
+  pub: string;
+  prv: string;
+  secret: string;
+}
 
 class Algo extends BaseCoin {
+  constructor() {
+    super();
+  }
 
-    constructor() {
-      super();
-    }
+  getChain(): string {
+    return 'algo';
+  }
 
-    getChain() {
-        return 'algo';
-    }
+  getFamily(): string {
+    return 'algo';
+  }
 
-    getFamily() {
-        return 'algo';
-    }
+  getFullName(): string {
+    return 'Algorand';
+  }
 
-    getFullName() {
-        return 'Algorand';
-    }
+  getBaseFactor(): number {
+    return 1e6;
+  }
 
-    getBaseFactor() {
-        return 1e6;
-    }
-
-    /**
+  /**
      * Flag for sending value of 0
      * @returns {boolean} True if okay to send 0 value, false otherwise
      */
-    valuelessTransferAllowed(): boolean {
-        // TODO: this sounds like its true with the staking txes - confirm before launch
-        return false;
+  valuelessTransferAllowed(): boolean {
+    // TODO: this sounds like its true with the staking txes - confirm before launch
+    return false;
+  }
+
+  /**
+   * Generate ed25519 key pair
+   *
+   * @param seed
+   * @returns {Object} object with generated pub, prv and secret
+   */
+  generateKeyPair(seed?: Buffer): KeyPair {
+    const pair = seed ? algosdk.generateAccountFromSeed(seed) : algosdk.generateAccount();
+    return {
+      pub: pair.addr, // encoded pub
+      prv: algosdk.Seed.encode(pair.sk), // encoded seed
+      secret: pair.sk, // secret as 64 byte long Uint8Array
+    };
+  }
+
+  /**
+   * Return boolean indicating whether input is valid public key for the coin.
+   *
+   * @param {String} pub the pub to be checked
+   * @returns {Boolean} is it valid?
+   */
+  isValidPub(pub): boolean {
+    return algosdk.isValidAddress(pub);
+  }
+
+  /**
+   * Return boolean indicating whether input is valid seed for the coin
+   * In Algorand, when the private key is encoded as base32 string only the first 32 bytes are taken,
+   * so the encoded value is actually the seed
+   *
+   * @param {String} prv the prv to be checked
+   * @returns {Boolean} is it valid?
+   */
+  isValidPrv(prv): boolean {
+    return algosdk.isValidSeed(prv);
+  }
+
+  /**
+   * Return boolean indicating whether input is valid public key for the coin
+   *
+   * @param {String} address the pub to be checked
+   * @returns {Boolean} is it valid?
+   */
+  isValidAddress(address): boolean {
+    return algosdk.isValidAddress(address);
+  }
+
+  /**
+   * Sign message with private key
+   *
+   * @param key
+   * @param message
+   */
+  signMessage(key, message): Buffer {
+    let seed = key.prv;
+    if (!this.isValidPrv(seed)) {
+      throw new Error(`invalid seed: ${seed}`);
     }
+    if (typeof seed === 'string') {
+      try {
+        seed = algosdk.Seed.decode(seed).seed;
+      } catch (e) {
+        throw new Error(`invalid seed: ${seed}`);
+      }
+    }
+    const keyPair = algosdk.generateAccountFromSeed(seed);
+
+    if (!Buffer.isBuffer(message)) {
+      message = Buffer.from(message);
+    }
+
+    return Buffer.from(algosdk.NaclWrapper.sign(message, keyPair.sk));
+  }
 }
 
 export default Algo;
---
+++ modules/core/test/v2/unit/coins/algo.ts
@@ -0,0 +1,60 @@
+const algosdk = require('algosdk');
+const crypto = require('crypto');
+
+const TestV2BitGo = require('../../../lib/test_bitgo');
+
+describe('ALGO:', function() {
+  let bitgo;
+  let basecoin;
+
+  before(function() {
+    bitgo = new TestV2BitGo({ env: 'test' });
+    bitgo.initializeTestVars();
+    basecoin = bitgo.coin('talgo');
+  });
+
+  it('should generate a keypair from random seed', function() {
+    const keyPair = basecoin.generateKeyPair();
+    keyPair.should.have.property('pub');
+    keyPair.should.have.property('prv');
+
+    const address = keyPair.pub;
+    basecoin.isValidAddress(address).should.equal(true);
+
+    basecoin.isValidPub(keyPair.pub).should.equal(true);
+    basecoin.isValidPrv(keyPair.prv).should.equal(true);
+  });
+
+  it('should generate a keypair from seed', function() {
+    const seed = crypto.randomBytes(32);
+    const keyPair = basecoin.generateKeyPair(seed);
+    keyPair.should.have.property('pub');
+    keyPair.should.have.property('prv');
+
+    const address = keyPair.pub;
+    basecoin.isValidAddress(address).should.equal(true);
+    basecoin.isValidPub(keyPair.pub).should.equal(true);
+    basecoin.isValidPrv(keyPair.prv).should.equal(true);
+
+    // verify regenerated keyPair from seed
+    const decodedSeed = algosdk.Seed.decode(keyPair.prv).seed;
+    const regeneratedKeyPair = basecoin.generateKeyPair(decodedSeed);
+
+    keyPair.pub.should.equal(regeneratedKeyPair.pub);
+    keyPair.prv.should.equal(regeneratedKeyPair.prv);
+    keyPair.secret.should.eql(regeneratedKeyPair.secret);
+  });
+
+  it('should validate address', function() {
+    const keyPair = basecoin.generateKeyPair();
+    basecoin.isValidAddress(keyPair.pub).should.equal(true);
+    basecoin.isValidPub(keyPair.pub).should.equal(true);
+    basecoin.isValidAddress('UMYEHZ2NNBYX43CU37LMINSHR362FT4GFVWL6V5IHPRCJVPZ46H6CBYLYX').should.equal(false);
+  });
+
+  it('should validate seed', function() {
+    const keyPair = basecoin.generateKeyPair();
+    basecoin.isValidPrv(keyPair.prv).should.equal(true);
+    basecoin.isValidPrv('UMYEHZ2NNBYX43CU37LMINSHR362FT4GFVWL6V5IHPRCJVPZ46H6CBYLYX').should.equal(false);
+  });
+});
